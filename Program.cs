4057eae7a90708c92b34e560cf5f9304243b2e4b
AlekseyBelyaev2606 <partchoice44@gmail.com>
2023-01-31 23:05:21 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Введите  число");
+int N = int.Parse(Console.ReadLine());
+if (N > 0 && N < 8)
+{
+    switch (N)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            Console.WriteLine("Будний день");
+            break;
+
+        case 6:
+        case 7:
+            Console.WriteLine("Выходной");
+            break;
+    }
+
+}
+else
+{
+    Console.WriteLine("Вы ввели неправильное число");
+}
